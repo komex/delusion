bd3b2b59531094face77853eea545f6c89609f67
Andrey Kolchenko <komexx@gmail.com>
2013-09-09 12:07:05 +0400

    Separate Delusion and Filter classes.

--- src/Delusion/Delusion.php
+++ src/Delusion/Delusion.php
@@ -77,7 +77,7 @@ class Delusion extends \php_user_filter
         $this->composer = $this->findComposer($autoloaders);
         spl_autoload_register([$this, 'loadClass'], true, true);
         $this->broker = new Broker(new Broker\Backend\Memory());
-        stream_filter_register('delusion.loader', __CLASS__);
+        stream_filter_register('delusion.loader', 'Delusion\\Filter');
     }
 
     /**
@@ -92,6 +92,37 @@ class Delusion extends \php_user_filter
         }
 
         return self::$instance;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPrefix()
+    {
+        return $this->prefix;
+    }
+
+    /**
+     * @return null|ReflectionClass
+     */
+    public function getCurrentClass()
+    {
+        if ($this->current_class === null) {
+            return null;
+        } else {
+            $class = $this->broker->getClass($this->current_class);
+            $this->current_class = null;
+
+            return $class;
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasCurrentClass()
+    {
+        return $this->current_class !== null;
     }
 
     /**
@@ -219,31 +250,6 @@ class Delusion extends \php_user_filter
     }
 
     /**
-     * Spoof class.
-     *
-     * @param resource $in
-     * @param resource $out
-     * @param int $consumed
-     * @param bool $closing
-     *
-     * @return int|void
-     */
-    public function filter($in, $out, &$consumed, $closing)
-    {
-        /** @var resource|object $bucket */
-        while ($bucket = stream_bucket_make_writeable($in)) {
-            if (self::$instance->current_class !== null) {
-                $bucket->data = $this->spoof();
-                self::$instance->current_class = null;
-                $consumed += strlen($bucket->data);
-                stream_bucket_append($out, $bucket);
-            }
-        }
-
-        return PSFS_PASS_ON;
-    }
-
-    /**
      * Get behavior of static class.
      *
      * @param string $class
@@ -259,24 +265,6 @@ class Delusion extends \php_user_filter
         }
 
         return $this->static_classes[$class];
-    }
-
-    /**
-     * Get behavior condition code.
-     *
-     * @param string $original_code
-     * @param bool $static
-     *
-     * @return string
-     */
-    private function getMethodSwitcher($original_code, $static)
-    {
-        return sprintf(
-            'if (%s) { %s } else { %s }',
-            $this->getMethodBehaviorCondition($static),
-            $this->getMethodReturnCode($static),
-            $original_code
-        );
     }
 
     /**
@@ -328,249 +316,6 @@ class Delusion extends \php_user_filter
         }
 
         return null;
-    }
-
-    /**
-     * Transform original class for getting full control.
-     *
-     * @return string
-     */
-    private function spoof()
-    {
-        $class = self::$instance->broker->getClass(self::$instance->current_class);
-        $code = $class->getFileReflection()->getSource();
-        if ($class->isInterface()) {
-            return $code;
-        } else {
-            /** @var ReflectionMethod[] $methods */
-            $methods = $class->getOwnMethods();
-            if (!empty($methods)) {
-                if (!$class->isTrait()) {
-                    $code = $this->addDelusionInterface($code, $class);
-                    $code = $this->addDelusionMethods($code, $methods[0]);
-                }
-                $code = $this->replaceMethods($code, $methods);
-            }
-
-            return $code;
-        }
-    }
-
-    /**
-     * Modified class must implements Delusion interface.
-     *
-     * @param string $code
-     * @param ReflectionClass $class
-     *
-     * @return string
-     */
-    private function addDelusionInterface($code, ReflectionClass $class)
-    {
-        $regexp = sprintf(
-            '/\bclass\s+%s(?:\s+(?:implements|extends)\s+[\w_\\,\s]+)?\s*{/im',
-            quotemeta(self::$instance->current_class)
-        );
-        $corrected = 'class ' . self::$instance->current_class;
-        if ($class->getParentClassName() != '') {
-            $corrected .= ' extends ' . $class->getParentClassName();
-        }
-        $interfaces = $class->getOwnInterfaceNames();
-        array_push($interfaces, '\\Delusion\\PuppetThreadInterface');
-        $interfaces = join(', ', array_unique($interfaces));
-        $corrected .= ' implements ' . $interfaces . ' {';
-
-        return preg_replace($regexp, $corrected, $code, 1);
-    }
-
-    /**
-     * Add methods for working with Delusion.
-     *
-     * @param string $code
-     * @param ReflectionMethod $method First method definition
-     *
-     * @return string
-     */
-    private function addDelusionMethods($code, ReflectionMethod $method)
-    {
-        $prefix = self::$instance->prefix;
-        $injected_code = <<<END
-    protected \${$prefix}invokes = [];
-    protected \${$prefix}returns = [];
-
-    public function delusionGetInvokesCount(\$method)
-    {
-        return count(\$this->delusionGetInvokesArguments(\$method));
-    }
-
-    public function delusionGetInvokesArguments(\$method)
-    {
-        return array_key_exists(\$method, \$this->{$prefix}invokes) ? \$this->{$prefix}invokes[\$method] : [];
-    }
-
-    public function delusionSetBehavior(\$method, \$returns)
-    {
-        \$this->{$prefix}returns[\$method] = \$returns;
-    }
-
-    public function delusionResetBehavior(\$method)
-    {
-        unset(\$this->{$prefix}returns[\$method]);
-    }
-
-    public function delusionHasCustomBehavior(\$method)
-    {
-        return array_key_exists(\$method, \$this->{$prefix}returns);
-    }
-
-    public function delusionResetAllBehavior()
-    {
-        \$this->{$prefix}returns = [];
-    }
-
-    public function delusionResetInvokesCounter(\$method)
-    {
-        unset(\$this->{$prefix}invokes[\$method]);
-    }
-
-    public function delusionResetAllInvokesCounter()
-    {
-        \$this->{$prefix}invokes = [];
-    }
-
-END;
-        $position = strpos($code, $method->getSource());
-
-        return substr($code, 0, $position) . $injected_code . substr($code, $position);
-    }
-
-    /**
-     * Replace methods in file to modified.
-     *
-     * @param string $code
-     * @param ReflectionMethod[] $methods
-     *
-     * @return string
-     */
-    private function replaceMethods($code, array $methods)
-    {
-        foreach ($methods as $method) {
-            if ($method->isAbstract()) {
-                continue;
-            }
-            $transformed_method = $this->methodInjector($method);
-            $code = str_replace($method->getSource(), $transformed_method, $code);
-        }
-
-        return $code;
-    }
-
-    /**
-     * Transform particular method.
-     *
-     * @param ReflectionMethod $method
-     *
-     * @return string
-     */
-    private function methodInjector(ReflectionMethod $method)
-    {
-        $source = $method->getSource();
-        $offset = (($method->getDocComment()) ? strlen($method->getDocComment()) : 0);
-        $start_position = strpos($source, '{', $offset) + 1;
-        $end_position = strrpos($source, '}');
-        $original_code = substr($source, $start_position, $end_position - $start_position);
-
-        $code = PHP_EOL . $this->getMethodDelusionClass();
-        $code .= $this->getMethodIncreaseInvokes($method->isStatic()) . PHP_EOL;
-        if ($method->isConstructor() || $method->isDestructor()) {
-            $code .= sprintf('if (!%s) { %s }', $this->getMethodBehaviorCondition(true), $original_code);
-        } else {
-            if (!$method->isStatic()) {
-                $original_code = $this->getMethodSwitcher($original_code, true);
-            }
-            $code .= $this->getMethodSwitcher($original_code, $method->isStatic());
-
-        }
-
-        return substr($source, 0, $start_position) . $code . substr($source, $end_position);
-    }
-
-    /**
-     * Get code which adds a variable with class behavior.
-     *
-     * @return string
-     */
-    private function getMethodDelusionClass()
-    {
-        $prefix = self::$instance->prefix;
-        $definition = sprintf('$%s = \Delusion\Delusion::injection();', $prefix) . PHP_EOL;
-
-        return $definition . sprintf('$%1$sclass = $%1$s->getClassBehavior(__CLASS__);', $prefix) . PHP_EOL;
-    }
-
-    /**
-     * Get code which uses in condition of what value will be returned.
-     *
-     * @param bool $static
-     *
-     * @return string
-     */
-    private function getMethodBehaviorCondition($static = false)
-    {
-        $prefix = self::$instance->prefix;
-        if ($static) {
-            return sprintf('$%sclass->delusionHasCustomBehavior(__FUNCTION__)', $prefix);
-        } else {
-            return sprintf('array_key_exists(__FUNCTION__, $this->%sreturns)', $prefix);
-        }
-    }
-
-    /**
-     * Get code which increases counter of method invokes.
-     *
-     * @param bool $static
-     *
-     * @return string
-     */
-    private function getMethodIncreaseInvokes($static = false)
-    {
-        $prefix = self::$instance->prefix;
-        if ($static) {
-            return sprintf('$%sclass->registerInvoke(__FUNCTION__, func_get_args());', $prefix);
-        } else {
-            return <<<END
-        if (empty(\$this->{$prefix}invokes[__FUNCTION__])) {
-            \$this->{$prefix}invokes[__FUNCTION__] = [];
-        }
-        array_push(\$this->{$prefix}invokes[__FUNCTION__], func_get_args());
-END;
-        }
-    }
-
-    /**
-     * Get code which returns custom value.
-     *
-     * @param bool $static
-     *
-     * @return string
-     */
-    private function getMethodReturnCode($static = false)
-    {
-        $prefix = self::$instance->prefix;
-        if ($static) {
-            $return_code = sprintf('$%1$sreturn = $%1$sclass->getCustomBehavior(__FUNCTION__);', $prefix);
-        } else {
-            $return_code = sprintf('$%1$sreturn = $this->%1$sreturns[__FUNCTION__];', $prefix);
-        }
-        $return_code .= <<<END
-
-            if (is_callable(\${$prefix}return)) {
-                return \${$prefix}return(func_get_args());
-            } else {
-                return \${$prefix}return;
-            }
-END;
-
-        return $return_code;
     }
 
     /**
