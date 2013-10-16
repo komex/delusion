933047a504282a18edb7694143c0eae6e10788a6
Andrey Kolchenko <komexx@gmail.com>
2013-10-16 23:06:17 +0400

    Recognize static methods.

--- src/Delusion/Modifier/MethodModifier.php
+++ src/Delusion/Modifier/MethodModifier.php
@@ -23,25 +23,43 @@ class MethodModifier extends Modifier
      * @var bool
      */
     protected $inHereDoc = false;
+    /**
+     * @var bool
+     */
+    protected $static = false;
 
+    /**
+     * @param int|string $type
+     * @param string $value
+     *
+     * @return string
+     */
     public function in($type, $value)
     {
-        if (!$this->inHereDoc) {
-            if ($type === '{') {
-                if ($this->balance === 0) {
-                    $value .= $this->getMethodCode();
-                }
-                $this->balance++;
-            } elseif ($type === '}') {
-                $this->balance--;
-                if ($this->balance < 0) {
-                    $value = $this->getDelusionMethods() . $value;
-                }
-            } elseif ($type === T_START_HEREDOC) {
-                $this->inHereDoc = true;
+        if ($this->inHereDoc) {
+            if ($type === T_END_HEREDOC) {
+                $this->inHereDoc = false;
             }
-        } elseif ($type === T_END_HEREDOC) {
-            $this->inHereDoc = false;
+
+            return $value;
+        }
+        if ($type === '{') {
+            if ($this->balance === 0) {
+                $value .= $this->getMethodCode();
+            }
+            $this->static = false;
+            $this->balance++;
+        } elseif ($type === '}') {
+            $this->balance--;
+            if ($this->balance < 0) {
+                $value = $this->getDelusionMethods() . $value;
+            }
+        } elseif ($type === T_STATIC) {
+            $this->static = true;
+        } elseif ($this->static && $type === T_VARIABLE) {
+            $this->static = false;
+        } elseif ($type === T_START_HEREDOC) {
+            $this->inHereDoc = true;
         }
 
         return $value;
